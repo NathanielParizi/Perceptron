9f5c15bf6b307aface6bc861149b48098e83bf75
justi <justi@DESKTOP-O7K8SPS>
2018-11-26 04:12:33 -0500

    Single-layered Perceptron

--- ANN/src/Perceptron.java
+++ ANN/src/Perceptron.java
@@ -10,8 +10,10 @@ public class Perceptron {
 
 		this.input = input;
 		this.output = output;
-		this.numOfWeights = input[0].length;
-		this.weights = new float[numOfWeights];
+		this.numOfWeights = input[0].length;	
+		this.weights = new float[numOfWeights];		
+		System.out.println("INPUTS:" + (this.input.length) + "\nOUTPUT:"+ this.output[0]
+				+ "\nNumber Of Weights:" + numOfWeights );
 
 		initializeWeight();
 
@@ -27,37 +29,45 @@ public class Perceptron {
 
 	public void train(float learningRate) {
 
-		float totalError = 1;
+		float totalError = 1;    //100% Error 
 		while (totalError != 0) {
 
 			totalError = 0;
 
 			for (int i = 0; i < output.length; i++) {
 
-				float calculatedOutput = calculateOutput(input[i]);
-				float error = output[i] - calculatedOutput; // this value can be positive with Math.abs() method
+				float perceptronOutput = perceptronOutput(input[i]);
+				 
+				float error = output[i] - perceptronOutput;            //  (t - o) = error
 
-				totalError += error;
+				totalError += error; //Adjust error
 
 				for (int j = 0; j < numOfWeights; j++) {
-					weights[j] = weights[j] + learningRate * input[i][j] * error;
-					System.out.println("Updated weight: " + weights[j]);
+					weights[j] = weights[j] + learningRate * error * input[i][j];     // n(t - o) = new weights
+					System.out.println("Training: Updated weight: " + weights[j]);    
 
 				}
 
 			}
-
-			System.out.println("Keep on training the network, error is : " + totalError);
+			
+			if(totalError == 0) {System.out.println("\n**************\nThe error has reached " + totalError);
+			
+			
+	
+			}
+			else {System.out.println("****************Keep on training the network, error is still: " + totalError);}
+			
 		}
 	}
 
-	public float calculateOutput(float[] input) {
+	//******* This calculates finds the sum of weights * input value of node and passes data to Activation Function
+	public float perceptronOutput(float[] input) {        
 
 		float sum = 0f;
 		for (int i = 0; i < input.length; i++) {
 			sum = sum + weights[i] * input[i];
 		}
-
+ 
 		return ActivationFunction.stepFunction(sum);
 	}
 }
--- ANN/src/SingleLayerPerceptron_Tester.java
+++ ANN/src/SingleLayerPerceptron_Tester.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 //*********************************************************************
 //	Nathaniel-Joel Parizi
 //	Machine Learning CSCI3900
@@ -9,25 +11,31 @@ public class SingleLayerPerceptron_Tester {
 
 	 public static void main(String[] args) {
 		 
-		 // LOGIC ************************************************************************
+		 // LOGIC DATA FOR TRAINING *****************************************************
 		 float[][] input = { {0,0}, {0,1}, {1,0}, {1,1} };   //Possible combinations
-		 float[] output = {0,0,0,1}; 	//AND LOGIC
+		 float[] output = {0,0,0,1}; 	// Target   (AND LOGIC)
+		 
+		 //float[] output = {0,0,0,1}; 	//AND LOGIC
 		 //float[] output = {0,1,1,1};  //OR LOGIC
-		 //float[] output = {0,0,0,1};  //XOR LOGIC     (Will never converge to error of 0!)
+		 //float[] output = {0,0,0,1};  //XOR LOGIC     (Will never converge to error of 0)
 		 //*******************************************************************************
 		 
 		 
-		 Perceptron perceptron = new Perceptron(input, output);
+		 Perceptron perceptron = new Perceptron(input, output);	 //Instantiate Perceptron Object
+		 float n = 0.1f;//  n  learningRate  used in n(t-o)x 
+		 perceptron.train(n);   
 		 
-		 perceptron.train(0.1f);
+		 System.out.println("The error is 0 so the neural network is ready! Predictions: ");
+		
+		
+		
+		 System.out.println(perceptron.perceptronOutput(new float[]{0,0}));
+		 System.out.println(perceptron.perceptronOutput(new float[]{0,1}));
+		 System.out.println(perceptron.perceptronOutput(new float[]{1,0}));
+		 System.out.println(perceptron.perceptronOutput(new float[]{1,1}));
 		 
-		 System.out.println("The error is 0 so our neural nnetwork is ready! Predictions: ");
 		 
-		 System.out.println(perceptron.calculateOutput(new float[]{0,0}));
-		 System.out.println(perceptron.calculateOutput(new float[]{0,1}));
-		 System.out.println(perceptron.calculateOutput(new float[]{1,0}));
-		 System.out.println(perceptron.calculateOutput(new float[]{1,1}));
-		 
+		
 	 }
 	
 }
